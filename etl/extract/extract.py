359ce86f6843ab7ddc78a0665485dafd5b42c51b
Maria Grozova <marija.grozova@gmail.com>
2025-05-14 16:18:25 +0100

    finalised extract file and ran to access required dataset size

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -18,25 +18,26 @@ def save_parks_metadata():
     if PARKS_FILE.exists():
         print("Parks metadata already exists. Skipping download.")
         return
+
     try:
         response = requests.get(PARKS_URL, timeout=10)
         response.raise_for_status()  # raises HTTPError for 4xx/5xx responses
         try:
             data = response.json()
         except json.JSONDecodeError as e:
-            print(f"Error decoding JSON for park_id {park_id}: {e}")
-            data = None
-        
+            print(f"Error decoding JSON for parks metadata file: {e}")
+            return
+
         with open(PARKS_FILE, "w", encoding="utf-8") as f:
             json.dump(response.json(), f, indent=2)
-            
+
         print("Saved original parks.json data.")
     except requests.RequestException as e:
         print(f"Error fetching parks metadata: {e}")
         raise
 
 
-# Get raw queue times for one park and return JSON with timestamp
+# Get current queue times for one park and return JSON with timestamp
 def get_queue_times_for_park(park_id):
     url = QUEUE_TEMPLATE.format(park_id)
     response = requests.get(url, timeout=10)
@@ -48,18 +49,24 @@ def get_queue_times_for_park(park_id):
     }
 
 
-# Define the number of iterations for fetching queue times
-FETCH_ITERATIONS = 12
+# Define the number of times to run fetching and appending queue times to the JSONL
+FETCH_ITERATIONS = 2
 
 # Fetch and append queue times to JSONL file
 def update_queue_times_every_5_minutes():
     with open(PARKS_FILE, "r", encoding="utf-8") as f:
         parks_data = json.load(f)
 
-    park_ids = [park["id"] for park in parks_data]
+    park_ids = []
+    for organization in parks_data:
+        if "parks" in organization:
+            park_ids.extend([park["id"] for park in organization["parks"]])
 
-    for i in range(FETCH_ITERATIONS):  # Run FETCH_ITERATIONS times (every 5 min for 30 minutes)
-        print(f"Fetching queue times iteration {i+1}/12...")
+    print(f"Total parks found: {len(park_ids)}")
+    print(f"Park IDs: {park_ids}")
+
+    for i in range(FETCH_ITERATIONS):  # Run FETCH_ITERATIONS times (every 5 min for the defined number of times)
+        print(f"Fetching queue times iteration {i+1}/2...")
 
         with open(QUEUE_FILE, "a", encoding="utf-8") as f:
             for park_id in park_ids:
