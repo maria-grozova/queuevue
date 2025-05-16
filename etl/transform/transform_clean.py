b4496064eecf1be35c298d5076b1635d407c004c
Maria Grozova <marija.grozova@gmail.com>
2025-05-16 16:38:13 +0100

    finish transform from json to csv and clean csv functions, trial running

--- etl/transform/transform_clean.py
+++ etl/transform/transform_clean.py
@@ -18,12 +18,12 @@ def clean_dataframe():
     except FileNotFoundError:
         print(f"Error: {ORIGINAL_DATAFRAME} not found.")
         return
+   
 
     # Drop unnecessary columns
     df = df.drop(columns=['company_id', 'company_name', 'timezone', 'timestamp', 'land_name', 'land_id'])
-    
+
     # Change data types
-    df['last_updated'] = pd.to_datetime(df['last_updated'], errors='coerce')
     df = df.astype({
         'park_id': 'int64',
         'park_name': 'string',
@@ -37,7 +37,8 @@ def clean_dataframe():
         'longitude': 'float64'
     })
     return df
-    
+
+
 # Transform the date to a more readable format 
 def transform_date_num(dt):
     """Transform the date number to include suffix"""
@@ -48,12 +49,37 @@ def transform_date_num(dt):
         suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10, 'th')
     return f"{day}{suffix} {dt.strftime('%B %Y')}"
 
+
 def format_date(df):
     """Format the date string to a more readable format."""
+    df['last_updated'] = pd.to_datetime(df['last_updated'], errors='coerce')
+
+    df['date'] = df['last_updated'].apply(
+        lambda dt: transform_date_num(dt) 
+        if pd.notnull(dt) 
+        else None
+        )
+    
+    df = df.drop(columns=['last_updated']) # Drop the original last_updated column as no longer needed
+    return df
+
+
+# Save the cleaned dataframe to a CSV file
+def save_dataframe(df):
+    """Save transformed dataframe"""
     try:
-        df['date'] = df['last_updated'].apply(format_date)
-        if pd.isnull(dt):
-            return None
-        return df
-    except ValueError:
-        return None
+        df.to_csv(CLEANED_FILE, index=False)
+        print(f"Cleaned dataframe saved to {CLEANED_FILE}")
+    except Exception as e:
+        print(f"Error saving cleaned dataframe: {e}")
+        return
+
+
+# Main function for cleaning
+def main():
+    df =  clean_dataframe()
+    df = format_date(df)
+    save_dataframe(df)
+
+if __name__ == "__main__":
+    main()
